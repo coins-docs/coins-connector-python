f077f96aff3c1d2f08f33362a970d763ab00f1b6
johncoins <149129667+johncoins@users.noreply.github.com>
2025-12-03 21:40:53 +0800

    update

--- config/conf.py
+++ config/conf.py
@@ -10,24 +10,13 @@ class Config(object):  # default config
         return self.__getattribute__(key)
 
 
-# class account1Config(Config):
-#     rest_url = 'https://api.pro.coins.ph'
-#     stream_url = "wss://wsapi.pro.coins.ph"
-#     invoice_key = ''
-#     invoice_secret = ''
-#     api_key = ''
-#     secret = ''
-
-
-
 class account1Config(Config):
-    rest_url = 'https://api.9001.pl-qa.coinsxyz.me'
-    stream_url = 'wss://ws.9001.pl-qa.coinsxyz.me'
-    invoice_key = 'fqcjLq92alD91V9tmLQbvpnO8IcAYGyo9BI4YsV0IsmitrnFwm7Gh9DC1xoxbi41'
-    invoice_secret = 'ryJvwV9nfqt5bzm76ESRredhaCCnuQh6jDtXAp20PlToZ8TM5xFoDr2ye8uWpH1r'
-    api_key = 'VGkCt1GWUqWsxsCtsTvqLP7xNxOikd6wd7uPbnMIk8RUHQZ2bNd4Gcmq6NgQ6VlK'
-    secret = '6AWutFs6ulhj8h1l51wnIt1efLVi13TE9SaHE4p76ovHbHWt782FPJMy7UgGxH1l'
-
+    rest_url = 'https://api.pro.coins.ph'
+    stream_url = "wss://wsapi.pro.coins.ph"
+    invoice_key = ''
+    invoice_secret = ''
+    api_key = ''
+    secret = ''
 
 
 
--- examples/RestAPI/fiat.py
+++ examples/RestAPI/fiat.py
@@ -5,31 +5,31 @@ import time
 timestamp = int(time.time() * 1000)
 
 
-# pprint(clent.fiat_support_channel(transaction_type='-1', currency='PHP'))
+pprint(clent.fiat_support_channel(transaction_type='-1', currency='PHP'))
 
-# pprint(fiatClient.fiat_cash_out(data={"internalOrderId": "16810937322990",
-#                                 "currency": "PHP",
-#                                 "amount": "50",
-#                                 "channelName": "SWIFTPAY_PESONET",
-#                                 "channelSubject": "gcash",
-#                                 "extendInfo": {"recipientName": "coins-open-api-test",
-#                                                "recipientAccountNumber": "123456789"}}))
+pprint(fiatClient.fiat_cash_out(data={"internalOrderId": "16810937322990",
+                                "currency": "PHP",
+                                "amount": "50",
+                                "channelName": "SWIFTPAY_PESONET",
+                                "channelSubject": "gcash",
+                                "extendInfo": {"recipientName": "coins-open-api-test",
+                                               "recipientAccountNumber": "123456789"}}))
 
-# pprint(clent.fiat_details(internal_order_id='16810937322990'))
-#
-# pprint(clent.fiat_history_order(page_num='1', page_size='10', internal_order_id='', transaction_type='', transaction_channel='', transaction_subject='', status='', fiat_currency='', start_date='', end_date=''))
-#
-# pprint(clent.generate_qr_code(requestId='', type='', source='', amount='', currency='PHP', remark='', expiredSeconds='', qrCodeMerchantName=''))
+pprint(clent.fiat_details(internal_order_id='16810937322990'))
 
-#
-# pprint(clent.get_qr_code(requestId='9805e40e-2c5a-4980-8023-53726c3885ec'))
-#
-# pprint(clent.fiat_history_order_v2(page_num='1', page_size='10', external_order_id='', internal_order_id='', transaction_type='', transaction_channel='', transaction_subject='', status='', fiat_currency='', start_date='', end_date=''))
-#
-# pprint(clent.generate_static_qr_code(request_id='9805e40e-2c5a-4980-8023-53726c3885ed', source='', currency='PHP', remark='', qr_code_merchant_name='', sub_merchant_id='', recv_window='', timestamp=timestamp))
-#
-# pprint(clent.cancel_qr_code(reference_id='', recv_window='', timestamp=timestamp))
-#
-# pprint(clent.update_qr_code(request_id='', status='', recv_window='', timestamp=timestamp))
-#
+pprint(clent.fiat_history_order(page_num='1', page_size='10', internal_order_id='', transaction_type='', transaction_channel='', transaction_subject='', status='', fiat_currency='', start_date='', end_date=''))
+
+pprint(clent.generate_qr_code(requestId='', type='', source='', amount='', currency='PHP', remark='', expiredSeconds='', qrCodeMerchantName=''))
+
+
+pprint(clent.get_qr_code(requestId='9805e40e-2c5a-4980-8023-53726c3885ec'))
+
+pprint(clent.fiat_history_order_v2(page_num='1', page_size='10', external_order_id='', internal_order_id='', transaction_type='', transaction_channel='', transaction_subject='', status='', fiat_currency='', start_date='', end_date=''))
+
+pprint(clent.generate_static_qr_code(request_id='9805e40e-2c5a-4980-8023-53726c3885ed', source='', currency='PHP', remark='', qr_code_merchant_name='', sub_merchant_id='', recv_window='', timestamp=timestamp))
+
+pprint(clent.cancel_qr_code(reference_id='', recv_window='', timestamp=timestamp))
+
+pprint(clent.update_qr_code(request_id='', status='', recv_window='', timestamp=timestamp))
+
 pprint(clent.get_qr_code_static_list(status='', recv_window='', timestamp=timestamp))
